5c6fde8a8764db5d1f594e707c5cb23bf3a8417f
LynS <61662998+lynsitu@users.noreply.github.com>
2022-09-20 21:41:27 -0700

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var examScore = 100
+    if (examScore < 60) {
+        println("You failed the class because your score is $examScore.")
+    } else if (examScore == 100 ) {
+        println("Wow! You nailed this exam!")
+    } else {
+        println("Congrats! You passed the exam!")
+    }
+}
